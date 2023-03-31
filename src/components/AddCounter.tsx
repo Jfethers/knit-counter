b06739fe8e6f296bcd3636c0b1159b27c8385f51
jfethers <jillfetherston@gmail.com>
2023-03-31 10:02:21 -0400

    add form and modal

---
+++ src/components/AddCounter.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, ReactNode, Ref, RefObject } from 'react';
+
+interface IAddCounterProps {}
+
+const AddCounter: FunctionComponent<IAddCounterProps> = (props: IAddCounterProps) => {
+
+    return (
+        <div className="form">
+
+        </div>
+    )
+}
+
+export default AddCounter;
