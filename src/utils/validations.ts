ccc29a5c068c965b53d4502934690bf438c11122
jfethers <jillfetherston@gmail.com>
2023-04-05 16:26:42 -0400

    clean up

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,6 +1,5 @@
 export const hex = (hex: string): string | undefined => {
     const hexRegex = /^(?:[0-9A-F]{3}|[0-9A-F]{6})$/i
-    console.log('hexRegex', hexRegex.test(hex));
     if (!hexRegex.test(hex)) {
         return 'Invalid hexidecimal value'
     }
